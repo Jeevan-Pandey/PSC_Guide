1718ee160f167fc75e27ee846600ff65cf3c1fcb
Jeevan Pandey <jeevanpandey520@gmail.com>
2024-08-12 10:43:05 +0545

    First Version

---
+++ script.js
@@ -0,0 +1,4 @@
+// Example: Simple alert for the Get Started button
+document.querySelector('.cta-button').addEventListener('click', function() {
+    alert('Get Started with Lok Sewa Abhyas!');
+});
